d6f073cedda37cc1fc704a3bb9bdb74af26f02e2
Mauricio Fow <mauriciofow@gmail.com>
2019-07-28 00:47:55 -0500

    Updates

---
+++ precache-manifest.8b59ce802d756fbad7b77cb92bba1327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16eb42af50152b35bc90828853695ac6",
+    "url": "/yugioh-deckbuilder/index.html"
+  },
+  {
+    "revision": "0ad293ae0309284758a8",
+    "url": "/yugioh-deckbuilder/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b5e7e43d85c953c1066c",
+    "url": "/yugioh-deckbuilder/static/css/main.ed6720e0.chunk.css"
+  },
+  {
+    "revision": "0ad293ae0309284758a8",
+    "url": "/yugioh-deckbuilder/static/js/2.84a53d53.chunk.js"
+  },
+  {
+    "revision": "b5e7e43d85c953c1066c",
+    "url": "/yugioh-deckbuilder/static/js/main.d4c481bf.chunk.js"
+  },
+  {
+    "revision": "30fd8eedb6a794c9161c",
+    "url": "/yugioh-deckbuilder/static/js/runtime~main.bdc8c11e.js"
+  }
+]);
